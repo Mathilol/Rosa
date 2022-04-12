2763304bd953df2cc1bed8938a34159ffe301d78
Mathilol <mathieu.judicq@gmail.com>
2022-04-12 09:15:44 +0200

    initial commit

---
+++ apps/patient-booking/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rosa-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'patient-booking';
+}
